69615815e6ab8742f389bcc3324312b0e9c65e3d
huangmeixiu <gugehuang@163.com>
2017-10-10 18:39:48 +0800

    resume

--- resume/src/vuex/actions.js
+++
@@ -1,8 +0,0 @@
-export default {
-  changeLanguage ({ commit, state }, lang) {
-    if (state.lang !== lang) {
-      commit('CHANGELANG', lang)
-      window.localStorage.setItem('x-language', lang)
-    }
-  }
-}
--- resume/src/vuex/mutations.js
+++
@@ -1,8 +0,0 @@
-import data from './data'
-
-export default {
-  CHANGELANG (state, lang) {
-    state.lang = lang
-    state.data = data[lang]
-  }
-}
